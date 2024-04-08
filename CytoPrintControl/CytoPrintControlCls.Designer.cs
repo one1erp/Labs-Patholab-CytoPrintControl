b682ccc27000439097a050b818d4bafb17e49b54
one1erp <avigaile@one1.co.il>
2024-04-08 17:19:18 +0300

    Initial commit with additional files

---
+++ CytoPrintControl/CytoPrintControlCls.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CytoPrintControl
+{
+    partial class CytoPrintControlCls
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBoxSisma = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelMisBdika = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblHeader.Location = new System.Drawing.Point(84, 26);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(512, 70);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "הדפסת מדבקות T5";
+            this.lblHeader.Click += new System.EventHandler(this.lblHeader_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBox1.Location = new System.Drawing.Point(52, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 30);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeDown);
+            // 
+            // textBoxSisma
+            // 
+            this.textBoxSisma.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.textBoxSisma.Location = new System.Drawing.Point(52, 189);
+            this.textBoxSisma.Name = "textBoxSisma";
+            this.textBoxSisma.Size = new System.Drawing.Size(200, 30);
+            this.textBoxSisma.TabIndex = 2;
+            this.textBoxSisma.Visible = false;
+            this.textBoxSisma.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBoxSisma_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(160, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "צנצנת";
+            // 
+            // labelMisBdika
+            // 
+            this.labelMisBdika.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelMisBdika.AutoSize = true;
+            this.labelMisBdika.Font = new System.Drawing.Font("Calibri", 12F);
+            this.labelMisBdika.Location = new System.Drawing.Point(31, 266);
+            this.labelMisBdika.Name = "labelMisBdika";
+            this.labelMisBdika.Size = new System.Drawing.Size(109, 24);
+            this.labelMisBdika.TabIndex = 11;
+            this.labelMisBdika.Text = "C000005/17";
+            this.labelMisBdika.Visible = false;
+            // 
+            // labelResult
+            // 
+            this.labelResult.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.labelResult.BackColor = System.Drawing.Color.Transparent;
+            this.labelResult.Font = new System.Drawing.Font("Arial", 25F, System.Drawing.FontStyle.Bold);
+            this.labelResult.ForeColor = System.Drawing.Color.Black;
+            this.labelResult.Location = new System.Drawing.Point(24, 513);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(559, 77);
+            this.labelResult.TabIndex = 12;
+            this.labelResult.Text = "אימות נכשל ";
+            this.labelResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelResult.Visible = false;
+            this.labelResult.Click += new System.EventHandler(this.labelResult_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.labelMisBdika);
+            this.panel1.Controls.Add(this.textBoxSisma);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(138, 137);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 320);
+            this.panel1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(88, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 35);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "סיסמת מנהל";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.lblHeader);
+            this.groupBox1.Controls.Add(this.labelResult);
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(34, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(602, 720);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(244, 628);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 60);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "יציאה";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CytoPrintControlCls
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CytoPrintControlCls";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Size = new System.Drawing.Size(703, 800);
+            this.Load += new System.EventHandler(this.CytoPrintControlCls_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBoxSisma;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelMisBdika;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ CytoPrintControl/CytoPrintControlCls.cs
@@ -0,0 +1,463 @@
+﻿using LSExtensionWindowLib;
+using LSSERVICEPROVIDERLib;
+using Patholab_Common;
+using Patholab_DAL_V1;
+using Patholab_XmlService;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+using System.Configuration;
+using System.Diagnostics;
+using UserControl = System.Windows.Forms.UserControl;
+using System.Timers;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace CytoPrintControl
+{
+
+
+    [ComVisible(true)]
+    [ProgId("CytoPrintControl.CytoPrintControlCls")]
+    public partial class CytoPrintControlCls : UserControl, IExtensionWindow
+    {
+
+        #region Private fields
+
+        private INautilusProcessXML xmlProcessor;
+        private IExtensionWindowSite2 _ntlsSite;
+        private INautilusServiceProvider sp;
+        private INautilusDBConnection _ntlsCon;
+
+        private DataLayer dal;
+        public bool DEBUG;
+        private string mboxHeader = "מסך אימות דגימה -  Nautilus";
+
+
+
+
+
+
+
+        #endregion
+
+        #region Ctor
+
+        public CytoPrintControlCls()
+        {
+            InitializeComponent();
+            //BackColor = Color.FromName("Control");
+            this.Dock = DockStyle.Fill;
+            label2.Visible = false;
+            labelMisBdika.Text = "";
+            labelResult.ForeColor = Color.Red;
+        }
+
+        #endregion
+
+
+
+        #region Implementation of IExtensionWindow
+
+        public bool CloseQuery()
+        {
+            if (dal != null) dal.Close();
+            this.Dispose();
+            return true;
+        }
+
+        public void Internationalise()
+        {
+        }
+
+        public void SetSite(object site)
+        {
+            _ntlsSite = (IExtensionWindowSite2)site;
+
+            _ntlsSite.SetWindowInternalName("CytoPrintControl");
+            _ntlsSite.SetWindowRegistryName("CytoPrintControl");
+            _ntlsSite.SetWindowTitle("הדפסת מדבקות T5");
+            labelResult.Visible = false;
+        }
+
+
+
+
+        public void PreDisplay()
+        {
+
+            xmlProcessor = Utils.GetXmlProcessor(sp);
+
+            Utils.GetNautilusUser(sp);
+
+            InitializeData();
+
+        }
+
+        public WindowButtonsType GetButtons()
+        {
+            return LSExtensionWindowLib.WindowButtonsType.windowButtonsNone;
+        }
+
+        public bool SaveData()
+        {
+            return false;
+        }
+
+        public void SetServiceProvider(object serviceProvider)
+        {
+            sp = serviceProvider as NautilusServiceProvider;
+            _ntlsCon = Utils.GetNtlsCon(sp);
+
+        }
+
+        public void SetParameters(string parameters)
+        {
+
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public WindowRefreshType DataChange()
+        {
+            return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        }
+
+        public WindowRefreshType ViewRefresh()
+        {
+            return LSExtensionWindowLib.WindowRefreshType.windowRefreshNone;
+        }
+
+        public void refresh()
+        {
+        }
+
+        public void SaveSettings(int hKey)
+        {
+        }
+
+        public void RestoreSettings(int hKey)
+        {
+        }
+
+        public void Close()
+        {
+
+        }
+
+        #endregion
+
+
+        #region Initilaize
+
+        public void InitializeData()
+        {
+
+
+            //    Debugger.Launch();
+            try
+            {
+                dal = new DataLayer();
+
+                if (DEBUG)
+                    dal.MockConnect();
+                else
+                    dal.Connect(_ntlsCon);
+
+
+
+
+            }
+            catch (Exception e)
+            {
+
+
+                MessageBox.Show("Error in  InitializeData " + "/n" + e.Message, mboxHeader);
+                Logger.WriteLogFile(e);
+            }
+
+        }
+
+
+
+
+
+
+        #endregion
+
+
+
+
+        private void textBox1_KeDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                LeaveText1();
+            }
+        }
+        private void textBoxSisma_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                textBoxSisma_Leave();
+            }
+
+        }
+        private void textBoxSisma_Leave()
+        {
+            try
+            {
+                string sql1 = "select phrase_description from lims_sys.PHRASE_ENTRY pe inner join lims_sys.PHRASE_HEADER ph  on PH.PHRASE_ID = PE.PHRASE_ID where PH.name = 'Cyto Manager' and pe.PHRASE_NAME = 'Password' and pe.phrase_description = '" + textBoxSisma.Text.Trim() + "'";
+
+                string desc = dal.GetDynamicStr(sql1);
+                if (!(desc is null))
+                {
+                    PrintControl();
+                    string sql5 = "select U_CYTO_PRINT_CONTROL_ID from lims_sys.U_CYTO_PRINT_CONTROL where NAME = '" + textBox1.Text.Trim().ToUpper() + "'";
+
+
+                    string pr_id = dal.GetDynamicStr(sql5);
+                    if (!(pr_id is null))
+                    {
+                        dal.UpdateCytoPrintControl(pr_id);
+                        dal.SaveChanges();
+                    }
+
+                }
+                else
+                {
+
+                    labelResult.Text = "סיסמת מנהל שגויה";
+                    _10SecStop();
+                    textBox1.Focus();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("textBoxSisma_Leave" + ex.Message);
+            }
+
+
+        }
+
+        private async Task _10SecStop()
+        {
+            try
+            {
+                labelResult.Visible = true;
+                textBox1.Enabled = false;
+
+                await Task.Delay(5000);
+
+
+                textBox1.Enabled = true;
+                textBox1.Text = String.Empty;
+                textBox1.Focus();
+                labelResult.ForeColor = Color.Red;
+                labelResult.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("_10SecStop" + ex.Message);
+            }
+        }
+
+        private long _sampleId = 0;
+        private long _sdgId;
+        private string s = "";
+        private void LeaveText1()
+
+        {
+
+            try
+            {
+                s = textBox1.Text.Trim().ToUpper();
+                if (s == String.Empty)
+                    return;
+
+                if (!s.StartsWith("C"))
+                {
+
+                    labelResult.Text = "לא נמצאה צנצנת עם הברקוד שהוזן";
+                    textBoxSisma.Text = "";
+                    _10SecStop();
+                    return;
+                }
+
+                SAMPLE currentSample;
+                string u_patholab_num = string.Empty;
+
+                currentSample = dal.FindBy<SAMPLE>(x => x.NAME == s).FirstOrDefault();
+                if (currentSample == null)
+                {
+                    labelResult.Text = "לא נמצאה צנצנת עם הברקוד שהוזן";
+                    _10SecStop();
+                    return;
+                }
+
+
+                _sdgId = currentSample.SDG.SDG_ID;
+                u_patholab_num = currentSample.SDG.SDG_USER.U_PATHOLAB_NUMBER;
+                labelMisBdika.Text = "מס' הבדיקה:  " + u_patholab_num;
+
+                _sampleId = currentSample.SAMPLE_ID;
+
+                string STATUS = currentSample.STATUS;
+                if (STATUS != "V" && STATUS != "A" && STATUS != "P" && STATUS != "I")
+                {
+
+                    labelResult.Text = "סטטוס צנצנת לא תואם";
+                    _10SecStop();
+                    return;
+                }
+
+
+
+
+
+                int numOfPrintings = 0;
+                try
+                {
+                    numOfPrintings = dal.FindBy<SDG_LOG>(x => x.SDG_ID == _sdgId &&
+                                        x.APPLICATION_CODE == "Cyt.Print" &&
+                                        (x.DESCRIPTION != null && x.DESCRIPTION.Contains(s))).Count();
+                }
+
+                catch
+                {
+
+                    //TO DO: bug, check reason.
+                    //MessageBox.Show("6");
+                }
+
+                if (numOfPrintings == 0 || numOfPrintings == null)
+                {
+                    PrintControl();
+
+                    currentSample.ALIQUOTs.ToList().ForEach(item => item.ALIQUOT_USER.U_ALIQUOT_STATION = "22");
+                    dal.SaveChanges();
+
+                }
+
+
+                else
+                {
+                    DateTime lastPrintingTime = DateTime.MinValue;
+                    if (numOfPrintings == 1)
+                    {
+
+                        lastPrintingTime = dal.FindBy<SDG_LOG>(x => x.SDG_ID == _sdgId && x.APPLICATION_CODE == "Cyt.Print" && x.DESCRIPTION.Contains(s))
+                            .FirstOrDefault().TIME;
+
+
+                        DateTime currentTime = DateTime.Now;
+                        TimeSpan timeDifference = currentTime.Subtract(lastPrintingTime);
+
+                        if (timeDifference.TotalMinutes >= 10)
+                        {
+                            PrintControl();
+
+                            currentSample.ALIQUOTs.ToList().ForEach(item => item.ALIQUOT_USER.U_ALIQUOT_STATION = "23");
+                            dal.SaveChanges();
+
+                        }
+                        else
+                        {
+                            labelResult.Visible = true;
+                            labelResult.Text = "המדבקה הודפסה ב10 הדקות האחרונות";
+                            textBoxSisma.Visible = false;
+                            _10SecStop();
+                            return;
+                        }
+
+                    }
+                    else
+                    {
+                        if (numOfPrintings >= 2)
+                        {
+                            textBoxSisma.Visible = true;
+                            textBox1.Enabled = false;
+                            labelResult.Visible = true;
+                            label2.Visible = true;
+                            labelResult.Text = "נדרשת סיסמת מנהל עבור הדפסה נוספת";
+                            textBoxSisma.Focus();
+                            labelResult.Visible = false;
+                        }
+
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("LeaveText1" + e.Message);
+                throw;
+            }
+        }
+        private void PrintControl()
+        {
+
+            try
+            {
+                Patholab_XmlService.FireEventXmlHandler fireEvent = new FireEventXmlHandler(sp);
+                fireEvent.CreateFireEventXml("SAMPLE", _sampleId, "Print Label");
+                bool returnbool = fireEvent.ProcssXml();
+
+                if (returnbool)
+                {
+                    dal.InsertToSdgLog(_sdgId, "Cyt.Print", (long)_ntlsCon.GetSessionId(), $"הדפסת מדבקה {s} בוצעה בהצלחה");
+                    dal.SaveChanges();
+                }
+                labelResult.Text = "הדפסת המדבקה בוצעה בהצלחה";
+                labelResult.ForeColor = Color.Green;
+                _10SecStop();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("PrintControl" + ex.Message);
+            }
+
+
+        }
+
+        private void labelResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblHeader_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CytoPrintControlCls_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _ntlsSite.CloseWindow();
+        }
+    }
+}
+
+
+
